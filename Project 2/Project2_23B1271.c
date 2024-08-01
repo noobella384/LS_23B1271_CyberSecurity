e1de8b784650e74771c6ba70d06e5bf69a69e021
noobella384 <162159734+noobella384@users.noreply.github.com>
2024-08-01 23:26:08 +0530

    Add files via upload

---
+++ Project 2/Project2_23B1271.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAPE_SIZE 30000
+
+void execute_brainfuck(const char *code) {
+    char tape[TAPE_SIZE] = {0};
+    char *ptr = tape;
+    const char *pc = code;
+   
+    while (*pc) {
+        switch (*pc) {
+            case '>':
+                ++ptr;
+                break;
+            case '<':
+                --ptr;
+                break;
+            case '+':
+                ++(*ptr);
+                break;
+            case '-':
+                --(*ptr);
+                break;
+            case '.':
+                putchar(*ptr);
+                break;
+            case ',':
+                *ptr = getchar();
+                break;
+            case '[':
+                if (*ptr == 0) {
+                    int loop = 1;
+                    while (loop > 0) {
+                        ++pc;
+                        if (*pc == '[') ++loop;
+                        if (*pc == ']') --loop;
+                    }
+                }
+                break;
+            case ']':
+                if (*ptr != 0) {
+                    int loop = 1;
+                    while (loop > 0) {
+                        --pc;
+                        if (*pc == ']') ++loop;
+                        if (*pc == '[') --loop;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        ++pc;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s \"<brainfuck code>\"\n", argv[0]);
+        return 1;
+    }
+
+    execute_brainfuck(argv[1]);
+
+    return 0;
+}
